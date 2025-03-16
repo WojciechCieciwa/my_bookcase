f0cdf52770d532f7cdd626ba1efad0e46174869f
WojciechCieciwa <wojciech.cieciwa@o2.pl>
2025-03-16 18:44:23 +0100

    zaczynam powoli budować szkielet przeglądarki...

--- py-gui-list/py-gui-lister-example1.py
+++ py-gui-list/py-gui-lister-example1.py
@@ -1,1 +1,33 @@
-import tkinter as tk
+from tkinter import *
+
+root = Tk()
+
+# tytul okna - bazowo nazwa programu, potem albo nazwa pliku z biblioteką, albo tytuł otwartej pozycji (albo ISBN)
+root.title("Tytuł okna - kiedyś będzie inny")
+
+# startowa wielkosc interfejsu bazowo SVGA
+root.geometry("1024x768")
+
+# tworzymy obszar roboczy aplikacji
+app = Frame(root)
+
+# a tu nie mam pojecie co i jak i dlaczego ? - ale tak stoi w książce to pewnie tak być powinno bo nie jest to wyjaśnione
+
+app.grid()
+
+# tworzymy etykiete - pozniej sie ja albo zmodyfikuje albo wywali teraz do testow jest
+lbl = Label(app,text ="taki sobie tekst etykiety ktorego zmienic nie mozna")
+
+# jak wyjasnic co to jewst grid? niby podswiadomie się orientuje to jakis 'wyzwalacz' ktory uruchamia  ale czy aby na pewno??
+lbl.grid()
+
+# no to zrobmy jakis przycisk a nie sama etykiete...
+bttnl = Button(app, text = "kolejny glupi tekst na nic nie robiącym przycisku.")
+# aktywujemy BTTNL
+bttnl.grid()
+
+
+
+
+
+root.mainloop()
